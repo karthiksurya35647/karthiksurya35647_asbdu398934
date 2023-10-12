3ef853f68b5a783520e0596bd32240ad85f75d73
karthiksurya35647 <147487184+karthiksurya35647@users.noreply.github.com>
2023-10-12 13:02:05 +0100

    Add files via upload

---
+++ main .1.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream> 
+using namespace std; 
+  
+// Driver Code 
+int main() 
+{ 
+    // Code Bloating 
+    string str("GeeksForGeeks"); 
+  
+    // Print string str 
+    cout << str << endl; 
+    return 0; 
+} 
